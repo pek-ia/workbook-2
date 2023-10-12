76b65800ddf98cd808fa496dc834ebb94cf1df08
Paul Kimball <pekimball@interfaceassociates.net>
2023-10-12 14:39:38 -0400

    Modified Main to add capitalization processing

--- FullNameGenerator/src/main/java/org/example/Main.java
+++ FullNameGenerator/src/main/java/org/example/Main.java
@@ -28,16 +28,20 @@ public class Main {
             System.out.println( message.charAt(i) );
         }
 
-        String first = getStringInput(scanner, "Enter your first name: ");
-        String middle = getStringInput(scanner, "Enter your middle name or initial: ");
-        String last = getStringInput(scanner, "Enter your last name: ");
-        String title = getStringInput(scanner, "Enter your title: ");
+        String first = getStringInput(scanner, "Enter your first name: ").trim();
+        String middle = getStringInput(scanner, "Enter your middle name or initial: ").trim();
+        String last = getStringInput(scanner, "Enter your last name: ").trim();
+        String title = getStringInput(scanner, "Enter your title: ").trim();
 
+        // Print out raw input without any spacing
         System.out.println(first + middle + last + title);
+
+        first = first.replaceFirst( first.substring(0,1), first.substring(0,1).toUpperCase());
 
         if ( middle.length() == 1 ) middle = middle + ".";
 
-        if ( ! title.isEmpty() ) title = ", " + title;
+        if ( ! title.isBlank() ) title = ", " + title;
+        title = title.toUpperCase();
 
         System.out.println(first + " " + middle + " " + last + title);
 
