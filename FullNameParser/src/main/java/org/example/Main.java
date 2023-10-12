b1d3bebe0c8b2d0316713272f6bdad54c57f5176
Paul Kimball <pekimball@interfaceassociates.net>
2023-10-12 14:58:31 -0400

    Initial commit

---
+++ FullNameParser/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] strings = {
+                "Hello", "goodbye"
+        };
+
+        for (String s: strings){
+            System.out.println(s);
+        }
+
+    }
+}
