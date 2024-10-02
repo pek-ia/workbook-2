d6ec9ad78f316341d7ab919e22e4d2996f6f53d8
Paul Kimball <pekimball@interfaceassociates.net>
2024-10-02 14:07:36 -0400

    Initial commit of StringAndLoops

---
+++ strings-and-loops/src/main/java/com/pluralsight/strings/StringsAndLoops.java
@@ -0,0 +1,8 @@
+package com.pluralsight.strings;
+
+public class StringsAndLoops {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.toLowerCase());
+    }
+}
